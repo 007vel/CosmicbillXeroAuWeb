45eafa28b42f055a3bfd116b24310505a55d07cc
007vel <sowmi071292@gmail.com>
2023-12-01 14:57:12 +0530

    alert message changes

--- src/app/my-account/my-account.component.ts
+++ src/app/my-account/my-account.component.ts
@@ -278,63 +278,64 @@ export class MyAccountComponent implements OnInit {
     buyWithCard() {
         debugger;
         var flag_purchase = false;
-        console.log("IsPaidPlan = "+this.subscribedPlan.IsPaidPlan);
+        console.log("IsPaidPlan = " + this.subscribedPlan.IsPaidPlan);
         console.log("total Paid Pdf = " + this.subscribedPlan.totalPaidPdf);
-        console.log("total Pdf Used = "+this.totalPdfUsed);
-        console.log("total Trial Pdf = "+this.subscribedPlan.TrialPdf);
-        console.log("total Trial Pdf Used = "+this.totalTrialPdfUsed);
-        if(this.subscribedPlan.IsPaidPlan){
-            if((this.subscribedPlan.totalPaidPdf - this.totalPdfUsed) > 0 ){
-                alert("You have Available PDF Count to sacn and use");
+        console.log("total Pdf Used = " + this.totalPdfUsed);
+        console.log("total Trial Pdf = " + this.subscribedPlan.TrialPdf);
+        console.log("total Trial Pdf Used = " + this.totalTrialPdfUsed);
+        if (this.subscribedPlan.IsPaidPlan) {
+            if ((this.subscribedPlan.totalPaidPdf - this.totalPdfUsed) > 0) {
+                alert("You have enough PDF Count to scan and use");
+
             }
-            else{
+            else {
                 debugger;
                 flag_purchase = true;
             }
         }
-        else{
-            if(!this.subscribedPlan.IsPaidPlan){
+        else {
+            if (!this.subscribedPlan.IsPaidPlan) {
                 debugger;
-                if((this.subscribedPlan.TrialPdf - this.totalTrialPdfUsed) > 0 ){
-                    alert("You have Available PDF Count to sacn and use");
+                if ((this.subscribedPlan.TrialPdf - this.totalTrialPdfUsed) > 0) {
+                    alert("You have enough PDF Count to scan and use");
                 }
-                else{
+                else {
                     debugger;
                     flag_purchase = true;
                 }
             }
         }
-        
-        if(flag_purchase){
+
+        if (flag_purchase) {
             debugger;
-                //------------
-                // Get Account Master Account By using Account ID 
-                // Check the IsXeroreferalUser Is 1 or 0 
-                // If 1 = True THen Xero Payment Link 
-                // If 0 = False Then Cosmic Payment Link
-                //------------
-                //   //
-                window.open("https://apps.xero.com/!sc-7l/au/subscribe/d589a79e-e0d5-483a-b129-c67d8327b808");
-                // var a = this.myAccountDetail.IsXeroReferaluser;
-                // if (a !== null) {
-                //     //  //
-                //     if (a) {
-                //         //For xerorefUder Link 
-                //         //    //
-                //         window.open("https://www.google.com/");
-                //     }
-                //     else {
-                //         if (!this.packagePurchaseHelper.CheckAvailablePaidPDFCount()) {
-                //             this.packagePurchaseHelper.NavigateToPackageApp();
-                //         } else {
-                //             alert('You have enough package to process bills..');
-                //         }
-                //     }
-                // }
+            //------------
+            // Get Account Master Account By using Account ID 
+            // Check the IsXeroreferalUser Is 1 or 0 
+            // If 1 = True THen Xero Payment Link 
+            // If 0 = False Then Cosmic Payment Link
+            //------------
+            //   //
+            window.open("https://apps.xero.com/!sc-7l/au/subscribe/d589a79e-e0d5-483a-b129-c67d8327b808");
+            // var a = this.myAccountDetail.IsXeroReferaluser;
+            // if (a !== null) {
+            //     //  //
+            //     if (a) {
+            //         //For xerorefUder Link 
+            //         //    //
+            //         window.open("https://www.google.com/");
+            //     }
+            //     else {
+            //         if (!this.packagePurchaseHelper.CheckAvailablePaidPDFCount()) {
+            //             this.packagePurchaseHelper.NavigateToPackageApp();
+            //         } else {
+            //             alert('You have enough package to process bills..');
+            //         }
+            //     }
+            // }
         }
 
-            
-        
+
+
     }
 
     AutoRenewalCheckboxChange(_event) {
